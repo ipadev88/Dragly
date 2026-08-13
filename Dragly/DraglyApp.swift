0a634183c905c532dca2e36105f92b1b76787898
ipadev <we1456545@ya.ru>
2026-08-13 13:58:41 +0300

    Initial Commit

---
+++ Dragly/DraglyApp.swift
@@ -0,0 +1,17 @@
+//
+//  DraglyApp.swift
+//  Dragly
+//
+//  Created by ipadev on 13.08.2026.
+//
+
+import SwiftUI
+
+@main
+struct DraglyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
